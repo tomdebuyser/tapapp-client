2a64803b32cfb2c1d4233e24c341de528df6d9c3
jellemannaerts <jelle.mannaerts@icapps.com>
2020-01-03 10:48:13 +0100

    Add translations, use bold, add import eslint rule

--- src/_shared/menu/Menu.test.tsx
+++ src/_shared/menu/Menu.test.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Menu from './Menu';
 import { translations } from '../../_translations';
 import { renderWithRouter } from '../../_utils/testHelpers';
+import Menu from './Menu';
 
 test('renders Menu', () => {
   const { getByText, getByAltText } = renderWithRouter(<Menu />);
-  const logo = getByAltText(translations.getLabel('SILVERNEXT'));
-  const usersLink = getByText(translations.getLabel('USERS'));
+  const logo = getByAltText(/silvernext/i);
+  const usersLink = getByText(translations.getLabel('NAVIGATION.USERS'));
   expect(logo).toBeInTheDocument();
   expect(usersLink).toBeInTheDocument();
 });
--- src/_shared/menu/Menu.tsx
+++ src/_shared/menu/Menu.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { translations } from '../../_translations';
 import slash from '../../_assets/images/slash-white.png';
+import { translations } from '../../_translations';
 import './menu.scss';
 
 const Menu = () => {
   return (
     <header className="main-menu">
-      <img src={slash} alt={translations.getLabel('SILVERNEXT')} />
+      <img src={slash} alt="Silvernext" />
       <nav>
-        <NavLink to={'/users'}>{translations.getLabel('USERS')}</NavLink>
+        <NavLink to={'/users'}>{translations.getLabel('NAVIGATION.USERS')}</NavLink>
       </nav>
     </header>
   );
