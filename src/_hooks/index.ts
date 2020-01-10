9eb531d0af6de4af072aefb778e3711253ba03d4
Tom De Buyser <tom.debuyser@icapps.com>
2020-01-10 16:47:50 +0100

    remove unused useSort

--- src/_hooks/index.ts
+++ src/_hooks/index.ts
@@ -3,8 +3,7 @@ import useForm from './useForm';
 import useInfiniteScroll from './useInfiniteScroll';
 import useInputError from './useInputError';
 import useModal from './useModal';
-import useSort from './useSort';
 import useTableSort from './useTableSort';
 import useToggle from './useToggle';
 
-export { useDebounce, useForm, useInfiniteScroll, useInputError, useModal, useSort, useTableSort, useToggle };
+export { useDebounce, useForm, useInfiniteScroll, useInputError, useModal, useTableSort, useToggle };
