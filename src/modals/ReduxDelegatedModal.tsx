ae09b18277e4ddb718ed6db749f32eb20e45987f
jellemannaerts <jelle.mannaerts@icapps.com>
2020-01-17 11:49:16 +0100

    Add FC

--- src/modals/ReduxDelegatedModal.tsx
+++ src/modals/ReduxDelegatedModal.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { FC } from 'react';
 import { useSelector } from 'react-redux';
 
 import { modalsSelectors } from '../_store/selectors';
 import { ModalType, IConfirmationModalData } from './_models';
 import ConfirmationModal from './confirmation/ConfirmationModal';
 
-const ReduxDelegatedModal = () => {
+const ReduxDelegatedModal: FC = () => {
   const isOpen = useSelector(modalsSelectors.isOpen);
   const data = useSelector(modalsSelectors.data);
   const type = useSelector(modalsSelectors.type);
