24fd19d7aedfe9d65c81374cff37a504aa9b12e3
Tom De Buyser <tom.debuyser@icapps.com>
2020-01-22 09:55:51 +0100

    remove newlines in modal selectors

--- src/modal/_store/selectors.ts
+++ src/modal/_store/selectors.ts
@@ -5,7 +5,5 @@ import { ModalState } from './reducer';
 const selectNode = (state: AppState) => state.modal;
 
 export const data = createSelector(selectNode, (state: ModalState) => state.data);
-
 export const isOpen = createSelector(selectNode, (state: ModalState) => state.isOpen);
-
 export const type = createSelector(selectNode, (state: ModalState) => state.type);
