8a12a07bff8099f3031e141397fc6350dfd5a105
Tom De Buyser <tom.debuyser@icapps.com>
2020-01-29 11:01:34 +0100

    added a config file for process.env variables + already foresee sentry integration

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,27 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
 import 'semantic-ui-less/semantic.less';
+import * as Sentry from '@sentry/browser';
 import App from './App';
+import { config } from './config';
 import { configureStore, history } from './_store/store';
 import * as serviceWorker from './serviceWorker';
 import './index.scss';
+
+// Setup sentry error logging
+const environmentsWithErrorLogging = ['production', 'staging'];
+const needsErrorLogging = config.sentryDsn() && environmentsWithErrorLogging.includes(config.environment());
+if (needsErrorLogging) {
+  Sentry.init({
+    dsn: config.sentryDsn(),
+    environment: config.environment(),
+  });
+}
+
+console.log('config.apiHost()', config.apiHost());
+console.log('config.environment()', config.environment());
+console.log('config.sentryDsn()', config.sentryDsn());
+console.log('config.projectName()', config.projectName());
 
 const store = configureStore();
 
