0d8b9ec53fb36dc498fcb36fb8810f3dca64d2e1
Tom De Buyser <tom.debuyser@icapps.com>
2020-03-16 10:30:18 +0100

    done not display createdBy/updatedBy if not available

--- src/_shared/timestamps/Timestamps.tsx
+++ src/_shared/timestamps/Timestamps.tsx
@@ -12,26 +12,18 @@ interface Props {
   };
 }
 
+function formatTimestamp(at: string, by: string, translationKey: string): string {
+  const params: Record<string, string> = { date: formatDate(dateFromISOString(at), 'dd/MM/yyyy HH:mm') };
+  if (by) params.name = by;
+  return translations.getLabel(`${translationKey}${by ? '_BY' : ''}`, params);
+}
+
 const Timestamps: FC<Props> = ({ entity }) => {
   const { createdAt, createdBy, updatedAt, updatedBy } = entity;
   return (
     <div className="timestamps">
-      {createdAt && (
-        <div>
-          {translations.getLabel('SHARED.DETAIL.CREATED_AT_BY', {
-            date: formatDate(dateFromISOString(createdAt), 'dd/MM/yyyy HH:mm'),
-            name: createdBy,
-          })}
-        </div>
-      )}
-      {updatedAt && (
-        <div>
-          {translations.getLabel('SHARED.DETAIL.UPDATED_AT_BY', {
-            date: formatDate(dateFromISOString(updatedAt), 'dd/MM/yyyy HH:mm'),
-            name: updatedBy,
-          })}
-        </div>
-      )}
+      {createdAt && <div>{formatTimestamp(createdAt, createdBy, 'SHARED.DETAIL.CREATED_AT')}</div>}
+      {updatedAt && <div>{formatTimestamp(updatedAt, updatedBy, 'SHARED.DETAIL.UPDATED_AT')}</div>}
     </div>
   );
 };
