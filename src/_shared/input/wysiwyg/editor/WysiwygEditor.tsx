e5c1462352965b740759d3015a6d4e666a818df4
Tom De Buyser <tom.debuyser@icapps.com>
2020-04-17 16:55:50 +0200

    wysiwyg styling

--- src/_shared/input/wysiwyg/editor/WysiwygEditor.tsx
+++ src/_shared/input/wysiwyg/editor/WysiwygEditor.tsx
@@ -4,6 +4,7 @@ import classnames from 'classnames';
 import { trim } from '../../../../_utils/formValidation';
 import 'react-quill/dist/quill.snow.css';
 import './wysiwygEditor.scss';
+import { useToggle } from '../../../../_hooks';
 
 // This represents the buttons and its options that will be visible in the toolbar
 // More info: https://quilljs.com/docs/modules/toolbar/
@@ -62,17 +63,20 @@ interface Props {
 const WysiwygEditor: FC<Props> = ({ disabled, error, onChange, placeholder, rows, value }) => {
   // We keep the content containing class names here to pass to the editor, otherwise it does not function well.
   const [content, setContent] = useState(value);
+  const [isActive, setIsActive] = useToggle(false);
 
   return (
     <ReactQuill
-      className={classnames({ error })}
+      className={classnames({ active: isActive, disabled, error })}
       formats={formats}
       modules={{ toolbar }}
+      onBlur={() => setIsActive(false)}
       onChange={(newContent: string, _delta, _source, editor: UnprivilegedEditor) => {
         const isEmpty = trim(editor.getText()).length === 0;
         setContent(newContent);
         onChange?.(isEmpty ? '' : replaceClassNames(newContent));
       }}
+      onFocus={() => setIsActive(true)}
       placeholder={placeholder}
       readOnly={disabled}
       style={{ height: `${rows * 3.4}rem` }}
