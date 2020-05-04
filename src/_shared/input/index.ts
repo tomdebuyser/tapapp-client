7ce0d5ea022f63d0fb3c7dc66e8ae1faf89425f3
Tom De Buyser <tom.debuyser@icapps.com>
2020-05-04 10:26:14 +0200

    add checkbox group wrapper

--- src/_shared/input/index.ts
+++ src/_shared/input/index.ts
@@ -1,4 +1,5 @@
 import Checkbox from './checkbox/Checkbox';
+import CheckboxGroup from './checkbox/CheckboxGroup';
 import Dropdown from './dropdown/Dropdown';
 import FileInput from './file/FileInput';
 import InputField from './inputField/InputField';
@@ -7,4 +8,4 @@ import Radio from './radio/Radio';
 import TextArea from './textArea/TextArea';
 import Wysiwyg from './wysiwyg/Wysiwyg';
 
-export { Checkbox, Dropdown, InputField, InputFieldDate, Radio, TextArea, FileInput, Wysiwyg };
+export { Checkbox, CheckboxGroup, Dropdown, InputField, InputFieldDate, Radio, TextArea, FileInput, Wysiwyg };
